e37649e4a963d563520037197b4280702f2356a4
Zachary Klosko <zklosko@hopecentral.com>
2025-11-15 16:47:01 -0500

    Feat: adding zone variables

--- variables.js
+++ variables.js
@@ -1,7 +1,20 @@
 module.exports = function (self) {
 	self.setVariableDefinitions([
-		{ variableId: 'variable1', name: 'My first variable' },
-		{ variableId: 'variable2', name: 'My second variable' },
-		{ variableId: 'variable3', name: 'Another variable' },
+		{ variableId: 'zone1int', name: 'Zone 1 Intensity' },
+		{ variableId: 'zone2int', name: 'Zone 2 Intensity' },
+		{ variableId: 'zone3int', name: 'Zone 3 Intensity' },
+		{ variableId: 'zone4int', name: 'Zone 4 Intensity' },
+		{ variableId: 'zone5int', name: 'Zone 5 Intensity' },
+		{ variableId: 'zone6int', name: 'Zone 6 Intensity' },
+		{ variableId: 'zone7int', name: 'Zone 7 Intensity' },
+		{ variableId: 'zone8int', name: 'Zone 8 Intensity' },
+		{ variableId: 'zone9int', name: 'Zone 9 Intensity' },
+		{ variableId: 'zone10int', name: 'Zone 10 Intensity' },
+		{ variableId: 'zone11int', name: 'Zone 11 Intensity' },
+		{ variableId: 'zone12int', name: 'Zone 12 Intensity' },
+		{ variableId: 'zone13int', name: 'Zone 13 Intensity' },
+		{ variableId: 'zone14int', name: 'Zone 14 Intensity' },
+		{ variableId: 'zone15int', name: 'Zone 15 Intensity' },
+		{ variableId: 'zone16int', name: 'Zone 16 Intensity' },
 	])
 }
