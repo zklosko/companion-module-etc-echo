00ee9054eb738a2d7f7bd2af1648db9da8b8adc6
Zachary Klosko <zklosko@hopecentral.com>
2025-11-15 16:46:16 -0500

    Feat: add UDP code from UDP module

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-const { InstanceBase, Regex, runEntrypoint, InstanceStatus } = require('@companion-module/base')
+const { InstanceBase, Regex, runEntrypoint, InstanceStatus, UDPHelper } = require('@companion-module/base')
 const UpgradeScripts = require('./upgrades')
 const UpdateActions = require('./actions')
 const UpdateFeedbacks = require('./feedbacks')
@@ -12,6 +12,8 @@ class ModuleInstance extends InstanceBase {
 	async init(config) {
 		this.config = config
 
+		await this.configUpdated(config)
+
 		this.updateStatus(InstanceStatus.Ok)
 
 		this.updateActions() // export actions
@@ -20,11 +22,56 @@ class ModuleInstance extends InstanceBase {
 	}
 	// When module gets deleted
 	async destroy() {
+		if (this.socket) {
+			this.socket.destroy()
+		} else if (this.udp) {
+			this.udp.destroy()
+		} else {
+			this.updateStatus(InstanceStatus.Disconnected)
+		}
+		
 		this.log('debug', 'destroy')
 	}
 
 	async configUpdated(config) {
+		if (this.udp) {
+			this.udp.destroy()
+			delete this.udp
+		}
+
+		if (this.socket) {
+			this.socket.destroy()
+			delete this.socket
+		}
+
 		this.config = config
+
+		this.init_udp()
+	}
+
+	init_udp() {
+		this.updateStatus(InstanceStatus.Connecting)
+
+		if (this.config.host) {
+			this.udp = new UDPHelper(this.config.host, this.config.port)
+			this.updateStatus(InstanceStatus.Ok)
+
+			this.udp.on('error', (err) => {
+				this.updateStatus(InstanceStatus.ConnectionFailure, err.message)
+				this.log('error', 'Network error: ' + err.message)
+			})
+
+			// If we get data, thing should be good (may need to be adjusted for EII response or lack thereof)
+			this.udp.on('listening', () => {
+				this.updateStatus(InstanceStatus.Ok)
+			})
+
+			this.udp.on('status_change', (status, message) => {
+				this.updateStatus(status, message)
+			})
+		} else {
+			this.updateStatus(InstanceStatus.BadConfig)
+		}
 	}
 
 	// Return config fields for web config
@@ -36,13 +83,17 @@ class ModuleInstance extends InstanceBase {
 				label: 'Target IP',
 				width: 8,
 				regex: Regex.IP,
+				required: true,
 			},
 			{
 				type: 'textinput',
 				id: 'port',
-				label: 'Target Port',
+				label: 'UDP Port',
+				default: '4703',
+				tooltip: 'Default port is 4703',
 				width: 4,
 				regex: Regex.PORT,
+				required: true,
 			},
 		]
 	}
